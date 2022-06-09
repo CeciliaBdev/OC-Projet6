a9feb6a29ea53165e9c9d8fc2bd2e85d43640f0a
CeciliaBdev <cecilia.bernard@live.fr>
2022-06-09 11:10:34 +0200

    ajout factory media

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,37 +1,50 @@
 function photographerFactory(data) {
     const { name, portrait, city, country, tagline, price,id} = data;
 
-    const picture = `assets/photographers/${portrait}`;
-
+    
     //creation des éléments (image-ville-pays...)
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture);
-        img.setAttribute('alt',name);
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        h2.setAttribute = ('aria-label', name);
-        const h3 = document.createElement ('h3');
-        h3.textContent = city +", "+country;
-        const p = document.createElement('p');
-        p.textContent = tagline;
-        const price_element = document.createElement('span')
-        price_element.textContent = price+"€/jour";
-        
+        const article = document.createElement('article');
 
-        //lien vers photgraphe.html
-        article.addEventListener("click", () => {
-            window.location.href = `photographer.html?id=${id}`;
-        })
+        article.innerHTML = `<a href="photographer.html?id=${id}">
+        <img src="assets/photographers/${portrait}">
+        <h2>${name}</h2>
+        </a>
+        <h3>${city}, ${country}</h3>
+        <p>${tagline}</p>
+        <p class="price">${price}€/jour</p>
+        `;
+        return article;
+    }
+   
+    return {  name, portrait, city, country, tagline, price,id, getUserCardDOM}
+}
 
+function mediaFactory(data){
+    const { id, photographerId, title, image, video, likes, date, price} = data;
+
+    function getMediaCardDom(){
+        const article = document.createElement("article");
+        const img = document.createElement("img");
+        img.setAttribute("src",`assets/images/${image}`);
 
         article.appendChild(img);
-        article.appendChild(h2);
-        article.appendChild(h3);
-        article.appendChild(p);
-        article.appendChild(price_element);
-        return (article);
+        
+        return article;
     }
-    return { name, picture, getUserCardDOM }
+
+    return {id, photographerId, title, image, video, likes, date, price, getMediaCardDom};
+
 }
+
+
+
+
+
+
+
+// //lien vers photgraphe.html
+        // article.addEventListener("click", () => {
+        //     window.location.href = `photographer.html?id=${id}`;
+        // })
+
